ec06c8629a283aa915b42f592137fc42b5e24215
Nikodem Zawirski <nikon96@gmail.com>
2020-12-26 22:30:02 +0100

    init

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Target;
+    [Range(0.5f, 2f)]
+    public float cameraHeight = 1f;
+    [Range(2f, 30f)]
+    public float offset = 7f;
+    [Range(0.001f, 1f)]
+    public float SmoothFactor = 1f;
+    public float rotationSpeed = 50f;
+    private Vector3 _cameraOffset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //zooming
+        float d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
+        {
+            // scroll up
+            offset -= 1;
+        }
+        else if (d < 0f)
+        {
+            // scroll down
+            offset += 1;
+        }
+        offset = Mathf.Clamp(offset, 2, 30);
+    }
+
+    void FixedUpdate()
+    {
+        _cameraOffset = -Target.forward * offset;
+        _cameraOffset.y = cameraHeight;
+        
+
+        Vector3 newPos = Target.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        Quaternion rotationToTarget = Quaternion.LookRotation(Target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float currentSteerAngle;
+    private float currentBrakeForce;
+    private bool isBraking;
+
+    public Transform centerOfMass;
+
+    [SerializeField] private float motorForce;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftWheelCollider;
+    [SerializeField] private WheelCollider frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider;
+    [SerializeField] private WheelCollider rearRightWheelCollider;
+
+    [SerializeField] private Transform frontLeftWheelTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform;
+    [SerializeField] private Transform rearRightWheelTransform;
+
+    public enum driveType { RWD, AWD }
+    public driveType drive = driveType.RWD;
+
+    public Text speedo;
+    public Text speedo1;
+
+    private Quaternion defaultRotation;
+    private Vector3 defaultPosition;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        defaultRotation = transform.rotation;
+        defaultPosition = transform.position;
+
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass.localPosition;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        UpdateWheels();
+
+        double speed = Math.Round(rb.velocity.magnitude * 3.6);
+        speedo.text = speed.ToString() + "km/h";
+
+        double wheelSpeed = Math.Round(((rearLeftWheelCollider.rpm * 2 * 3.14 * rearLeftWheelCollider.radius) / 60) * 3.6);
+        speedo1.text = wheelSpeed.ToString() + "km/h";
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMotor();
+        HandleSteering();
+    }
+
+
+    private void GetInput()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.rotation = defaultRotation;
+            transform.position = defaultPosition;
+        }
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticalInput = Input.GetAxis(VERTICAL);
+        isBraking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleMotor()
+    {
+        if(drive == driveType.RWD){
+            rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+            rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+        }
+        else
+        {
+            rearLeftWheelCollider.motorTorque = verticalInput * motorForce;
+            rearRightWheelCollider.motorTorque = verticalInput * motorForce;
+            frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+            frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        }
+
+        currentBrakeForce = 0f;
+
+        currentBrakeForce = isBraking ? brakeForce : currentBrakeForce;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        frontRightWheelCollider.brakeTorque = currentBrakeForce;
+        frontLeftWheelCollider.brakeTorque = currentBrakeForce;
+        rearLeftWheelCollider.brakeTorque = currentBrakeForce;
+        rearRightWheelCollider.brakeTorque = currentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot; 
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
